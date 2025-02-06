3c840b6f2656b2beabb7056939f12b7d66b2ede1
Jose Muzquiz <jamuzquizgarcia@gmail.com>
2025-02-06 12:32:33 -0800

    finished exercise

--- dist/ex04.js
+++ dist/ex04.js
@@ -1,13 +1,12 @@
+"use strict";
 // Create a function called greet.
 // This function should accept a required name (string) and an optional greeting message (string).
 // If no greeting is provided, use "Hello" as the default message.
 // The function should log the greeting message followed by the name. Specify the correct return type for logging.
-
-function greet(name: string, message?: string): void {
-    message ? 
-    console.log(`${message}, ${name}`)
-    : console.log(`Hello, ${name}`)
+function greet(name, message) {
+    message ?
+        console.log(`${message}, ${name}`)
+        : console.log(`Hello, ${name}`);
 }
-
-greet("John");               // Expected output: Hello, John!
+greet("John"); // Expected output: Hello, John!
 greet("John", "Good morning"); // Expected output: Good morning, John!
--- dist/ex05.js
+++ dist/ex05.js
@@ -1,13 +1,12 @@
+"use strict";
 // Create a function called sumAllNumbers.
 // This function should accept any number of numbers using a rest parameter.
 // It should return the sum of all the numbers passed to it.
-
-function sumAllNumbers(...numbers: number[] ): number {
-    let result = 0
+function sumAllNumbers(...numbers) {
+    let result = 0;
     numbers.forEach(number => {
-        result = result + number        
+        result = result + number;
     });
-    return result
+    return result;
 }
-
 console.log(sumAllNumbers(1, 2, 3, 4, 5)); // Expected output: 15
